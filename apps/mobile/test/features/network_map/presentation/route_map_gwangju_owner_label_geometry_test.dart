36d74088324f7fd0a18124cb34a2da9de8119b66
Aquila <illusiveman7@gmail.com>
2026-08-11 23:21:20 +0900

    refactor(mobile): move owner label bounds (#19) (#167)

--- apps/mobile/test/features/network_map/presentation/route_map_gwangju_owner_label_geometry_test.dart
+++ apps/mobile/test/features/network_map/presentation/route_map_gwangju_owner_label_geometry_test.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 
 import 'package:easysubway_mobile/features/network_map/domain/route_map_design_space.dart';
 import 'package:easysubway_mobile/features/network_map/domain/route_map_owner_labels.dart';
+import 'package:easysubway_mobile/features/network_map/presentation/route_map_owner_label_bounds.dart';
 import 'package:easysubway_mobile/features/network_map/presentation/route_map_basemap_view.dart';
 import 'package:easysubway_mobile/features/route_draft/application/route_draft_controller.dart';
 import 'package:easysubway_mobile/features/network_map/domain/network_map_models.dart';
