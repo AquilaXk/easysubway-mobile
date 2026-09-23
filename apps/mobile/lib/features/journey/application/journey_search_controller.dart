e42eb033e00a27ec20d13e0dd5ea3f03d565f967
AquilaXk <illusiveman7@gmail.com>
2026-09-23 20:26:40 +0900

    feat(journey): enable waypoint search command and pass viaStationId to Journey V3 API

--- apps/mobile/lib/features/journey/application/journey_search_controller.dart
+++ apps/mobile/lib/features/journey/application/journey_search_controller.dart
@@ -95,6 +95,7 @@ class JourneySearchCommand {
   const JourneySearchCommand({
     required this.originStationId,
     required this.destinationStationId,
+    this.viaStationId,
     required this.departure,
     required this.timePolicy,
     required this.walkingPace,
@@ -106,6 +107,7 @@ class JourneySearchCommand {
 
   final String originStationId;
   final String destinationStationId;
+  final String? viaStationId;
   final JourneyDeparture departure;
   final TimePolicy timePolicy;
   final WalkingPace walkingPace;
@@ -119,6 +121,7 @@ class JourneySearchCommand {
       other is JourneySearchCommand &&
       originStationId == other.originStationId &&
       destinationStationId == other.destinationStationId &&
+      viaStationId == other.viaStationId &&
       departure.toJson().toString() == other.departure.toJson().toString() &&
       timePolicy == other.timePolicy &&
       walkingPace == other.walkingPace &&
@@ -131,6 +134,7 @@ class JourneySearchCommand {
   int get hashCode => Object.hash(
     originStationId,
     destinationStationId,
+    viaStationId,
     departure.toJson().toString(),
     timePolicy,
     walkingPace,
@@ -305,6 +309,7 @@ class JourneySearchController extends ChangeNotifier {
         requestId: _requestIdGenerator(),
         originStationId: command.originStationId,
         destinationStationId: command.destinationStationId,
+        viaStationId: command.viaStationId,
         departure: command.departure,
         timePolicy: command.timePolicy,
         walkingPace: command.walkingPace,
